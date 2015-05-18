bfdb7d2bdfc631f367565cdec37d85cced86e332
Sandro Groganz <sandro@campaignchain.com>
2015-05-18 15:20:16 -0700

    CE-299 Campaign template

--- Controller/ShareNewsItemController.php
+++ Controller/ShareNewsItemController.php
@@ -270,7 +270,7 @@ class ShareNewsItemController extends Controller
 
         $data = $request->get('campaignchain_core_activity');
 
-        $responseData['data'] = $data;
+        // $responseData['payload'] = $data;
 
         $activityService = $this->get('campaignchain.core.activity');
         $activity = $activityService->getActivity($id);
@@ -302,6 +302,10 @@ class ShareNewsItemController extends Controller
 //            // TODO: Add different flashbag which includes link to posted message on Facebook
 //            // TODO: If this previously was a scheduled activity, then reset the schedule
 //        }
+
+        $responseData['start_date'] =
+        $responseData['end_date'] =
+            $activity->getStartDate()->format(\DateTime::ISO8601);
 
         $encoders = array(new JsonEncoder());
         $normalizers = array(new GetSetMethodNormalizer());
